b733e3ae5b0fbc7651896b7086005cad070f1b5c
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-08-18 13:16:32 +0300

    feat: add new cities

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -4,15 +4,61 @@ export const citiesData: ICity[] = [
   {
     id: 1,
     name: 'Минск / Мінск',
-    country: 'Беларусь',
     coatOfArmsUrl: ' ../coats-of-arms/Minsk.svg',
     aliases: ['минск', 'мінск', 'minsk'],
   },
   {
     id: 2,
     name: 'Гродно / Горадня',
-    country: 'Беларусь',
     coatOfArmsUrl: '/coats-of-arms/Hrodna.svg',
     aliases: ['гродно', 'горадне', 'гародня', 'hrodna'],
-  }
+  },
+  {
+    id: 3,
+    name: 'Заславль / Заслаўе',
+    coatOfArmsUrl: '/coats-of-arms/Zaslauje.svg',
+    aliases: ['заславль', 'заслаўе', 'zaslavl', 'zaslauje'],
+  },
+  {
+    id: 4,
+    name: 'Пинск / Пінск',
+    coatOfArmsUrl: '/coats-of-arms/Pinsk.svg',
+    aliases: ['пинск', 'пінск', 'pinsk'],
+  },
+  {
+    id: 5,
+    name: 'Лида / Ліда',
+    coatOfArmsUrl: '/coats-of-arms/Lida.svg',
+    aliases: ['лида', 'ліда', 'lida'],
+  },
+  {
+    id: 6,
+    name: 'Полоцк / Полацк',
+    coatOfArmsUrl: '/coats-of-arms/Polack.svg',
+    aliases: ['полоцк', 'полацк', 'polotsk', 'polack'],
+  },
+  {
+    id: 7,
+    name: 'Брест / Берасце',
+    coatOfArmsUrl: '/coats-of-arms/Brest.svg',
+    aliases: ['брест', 'брэст', 'берасце', 'берестье', 'brest'],
+  },
+  {
+    id: 8,
+    name: 'Витебск / Віцебск',
+    coatOfArmsUrl: '/coats-of-arms/Viciebsk.svg',
+    aliases: ['витебск', 'віцебск', 'vitebsk'],
+  },
+  {
+    id: 9,
+    name: 'Гомель / Гомель',
+    coatOfArmsUrl: '/coats-of-arms/Homiel.svg',
+    aliases: ['гомель', 'gomel', 'homel'],
+  },
+   {
+    id: 10,
+    name: 'Смолевичи / Смалявичi',
+    coatOfArmsUrl: '/coats-of-arms/Smalavicy.svg',
+    aliases: ['смолевичи', 'Смалявичi', 'smalavicy'],
+  },
 ];
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 export interface ICity {
   id: number;
   name: string; 
-  country: string; 
   coatOfArmsUrl: string; 
   aliases: string[]; 
 }
